78628aff4259b583984d5ce9adc9a96db40766ea
lf833372 <lf833372@gmail.com>
2021-06-30 18:34:41 -0500

    wpm and time added

--- src/App.tsx
+++ src/App.tsx
@@ -1,111 +1,142 @@
-import { useEffect, useRef, useState } from "react";
+import { ChangeEvent, useEffect, useRef, useState } from "react";
 import WordButton from "./components/WordButton";
 import Layout from "./components/Layout";
 import Word from "./components/Word";
 import { getWordsByLength } from "./utils/words";
 
-import {FaCheck, FaRedoAlt} from "react-icons/fa"
+import { FaCheck, FaRedoAlt } from "react-icons/fa";
 
 import "./App.css";
+import { useInterval } from "./hooks/useInterval";
+import { useCallback } from "react";
 
 const App = () => {
-  const [words, setWords] = useState<Array<string>>([]);
-  const [current, setCurrent] = useState(0);
-  const [wrongs, setWrongs] = useState<Array<number>>([])
-  const [value, setValue] = useState("");
-  const [wordsNumber, setWordsNumber] = useState(25);
-  const inputRef = useRef<HTMLInputElement>(null);
-  const [finish, setFinish] = useState(false);
-  const [start, setStart] = useState(false);
+    const [words, setWords] = useState<Array<string>>([]);
+    const [current, setCurrent] = useState<number>(0);
+    const [wrongs, setWrongs] = useState<Array<number>>([]);
+    const [value, setValue] = useState<string>("");
+    const [wordsNumber, setWordsNumber] = useState<number>(25);
+    const inputRef = useRef<HTMLInputElement>(null);
+    const [finish, setFinish] = useState<boolean>(false);
+    const [start, setStart] = useState<boolean>(false);
+    const [wpm, setWpm] = useState<number>(0);
+    const [time, setTime] = useState<number>(0);
+    const [typed, setTyped] = useState<number>(0);
 
-  const handleRestart = () => {
-    setStart(false);
-    setFinish(false);
-    setCurrent(0);
-    setWrongs([]);
-    setValue("");
-    inputRef.current?.focus()
-    setWords(getWordsByLength(wordsNumber));
-  }
+    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setValue(e.target.value);
+        setTyped((t) => t + 1);
+    };
 
-  useEffect(() => {
-    setStart(false);
-    setFinish(false);
-    setCurrent(0);
-    setWrongs([]);
-    setValue("");
-    inputRef.current?.focus()
-    setWords(getWordsByLength(wordsNumber));
-  }, [wordsNumber])
+    const updateWPM = useCallback(() => {
+        if (!time || !typed) return;
+        setWpm(Math.floor(((typed / time) * 60) / wordsNumber));
+    }, [time, typed, wordsNumber]);
 
-  useEffect(() => {
-    if(!value.length) return;
-    const word = value.trim();
-    const space = value[value.length-1] === ' ';
-    if(word.length) setStart(true);
-    if(space) {
+    useInterval(() => {
+        if (!start) return;
+        setTime((t) => t + 1);
+    }, 1000);
 
-      if(word !== words[current]) {
-        setWrongs(w => [...w, current]);
-      }
-      setCurrent(current + 1);
-      setValue("");
-    } else if(current === words.length - 1 && word.length === words[words.length - 1].length) {
-      setFinish(true);
-      setValue("");
-    }
-  }, [value, current, words])
+    const handleRestart = () => {
+        setTime(0);
+        setWpm(0);
+        setTyped(0);
+        setStart(false);
+        setFinish(false);
+        setCurrent(0);
+        setWrongs([]);
+        setValue("");
+        inputRef.current?.focus();
+        setWords(getWordsByLength(wordsNumber));
+    };
 
+    useEffect(() => {
+        setTime(0);
+        setTyped(0);
+        setStart(false);
+        setFinish(false);
+        setCurrent(0);
+        setWrongs([]);
+        setValue("");
+        inputRef.current?.focus();
+        setWords(getWordsByLength(wordsNumber));
+    }, [wordsNumber]);
 
-  return (
-    <Layout>
-      {/* {start && <p>start</p>} */}
-      <div className="controls">
-        <div>
-          <WordButton value={10} wordsNumber={wordsNumber} setWordsNumber={setWordsNumber} />
-          <WordButton value={25} wordsNumber={wordsNumber} setWordsNumber={setWordsNumber} />
-          <WordButton value={50} wordsNumber={wordsNumber} setWordsNumber={setWordsNumber} />
-          <WordButton value={100} wordsNumber={wordsNumber} setWordsNumber={setWordsNumber} />
-          <WordButton value={250} wordsNumber={wordsNumber} setWordsNumber={setWordsNumber} />
-        </div>
-        {finish && (
-          <div>
-            <p className="info">{start && <span></span>}<span className="finish"><FaCheck /></span></p>
-          </div>
-        )}
-      </div>
+    useEffect(() => {
+        if (!value.length) return;
+        const word = value.trim();
+        const space = value[value.length - 1] === " ";
+        if (word.length) setStart(true);
+        if (space) {
+            if (word !== words[current]) {
+                setWrongs((w) => [...w, current]);
+            }
+            setCurrent(current + 1);
+            setValue("");
+        } else if (current === words.length - 1 && word.length === words[words.length - 1].length) {
+            setFinish(true);
+            setStart(false);
+            setValue("");
+            if (word !== words[current]) {
+                setWrongs((w) => [...w, current]);
+            }
+        }
+        updateWPM();
+    }, [value, current, words, updateWPM]);
 
-      <div className="text">
-        {words.map((word, i) => <Word key={i} word={word} current={i === current} wrong={wrongs.includes(i)} />)}
-      </div>
+    return (
+        <Layout>
+            <div className="controls">
+                <div>
+                    {[10, 25, 50, 100, 250].map((val) => (
+                        <WordButton
+                            value={val}
+                            wordsNumber={wordsNumber}
+                            setWordsNumber={setWordsNumber}
+                        />
+                    ))}
+                </div>
+                <div>
+                    <p className="info">
+                        {finish && (
+                            <span className="finish">
+                                <FaCheck />
+                            </span>
+                        )}
+                        <span className="label">
+                            {wpm}
+                            <span className="nolabel"> wpm</span>
+                        </span>
+                        <span className="label">
+                            {time}
+                            <span className="nolabel"> sec</span>
+                        </span>
+                    </p>
+                </div>
+            </div>
 
-      <div className="inputs">
-        <input
-          style={{
-            flexGrow: 1,
-            padding: ".3rem",
-            backgroundColor: "rgba(255, 255, 255, .1)",
-            fontSize: "1.1rem",
-            outline: "none",
-            color: "#fff",
-            border: "none",
-            borderRadius: "5px",
-            fontFamily: "Roboto Mono"
-          }}
-          ref={inputRef}
-          type="text"
-          value={value}
-          onChange={(e) => setValue(e.target.value)}
-          readOnly={finish}
-        />
-        <button className="restart"
-          onClick={handleRestart}
-        >
-          <FaRedoAlt />
-        </button>
-      </div>
-    </Layout>
-  )
-}
+            <div className="text">
+                {words.map((word, i) => (
+                    <Word key={i} word={word} current={i === current} wrong={wrongs.includes(i)} />
+                ))}
+            </div>
+
+            <div className="inputs">
+                <input
+                    className="input"
+                    ref={inputRef}
+                    type="text"
+                    value={value}
+                    onChange={handleChange}
+                    readOnly={finish}
+                />
+                <button className="restart" onClick={handleRestart}>
+                    <FaRedoAlt />
+                </button>
+            </div>
+        </Layout>
+    );
+};
 
 export default App;
--- src/hooks/useInterval.tsx
+++ src/hooks/useInterval.tsx
@@ -1,21 +1,21 @@
-import React, { useState, useEffect, useRef } from 'react';
+import { useEffect, useRef } from "react";
 
 export function useInterval(callback: () => void, delay: number) {
-  const savedCallback = useRef<any>();
+    const savedCallback = useRef<any>();
 
-  // Remember the latest callback.
-  useEffect(() => {
-    savedCallback.current = callback;
-  }, [callback]);
+    // Remember the latest callback.
+    useEffect(() => {
+        savedCallback.current = callback;
+    }, [callback]);
 
-  // Set up the interval.
-  useEffect(() => {
-    function tick() {
-      savedCallback.current();
-    }
-    if (delay !== null) {
-      let id = setInterval(tick, delay);
-      return () => clearInterval(id);
-    }
-  }, [delay]);
+    // Set up the interval.
+    useEffect(() => {
+        function tick() {
+            savedCallback.current();
+        }
+        if (delay !== null) {
+            let id = setInterval(tick, delay);
+            return () => clearInterval(id);
+        }
+    }, [delay]);
 }
